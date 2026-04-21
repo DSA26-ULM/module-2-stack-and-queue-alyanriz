6b9d066f9a6bc3abc54c4147e6dfcdae97b443db
Nayla Rizkia <naylarizkvia@gmail.com>
2026-04-21 21:35:45 +0800

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -12,3 +12,25 @@ bool isEmpty(const Stack* s) {
 bool isFull(const Stack* s) {
     return s->top >= s->data + MAX - 1;
 }
+
+void push(Stack* s, int value) {
+    if (isFull(s)) {
+        throw std::overflow_error("Stack sudah penuh.");
+    }
+    s->top++; 
+    *(s->top) = value; 
+}
+
+void pop(Stack* s) {
+    if (isEmpty(s)) {
+        throw std::underflow_error("Stack kosong.");
+    }
+    s->top--; 
+}
+
+int peek(const Stack* s) {
+    if (isEmpty(s)) {
+        throw std::underflow_error("Stack kosong, tidak ada data.");
+    }
+    return *(s->top); 
+}
