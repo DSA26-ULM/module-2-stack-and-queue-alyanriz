883d34a2f8a7a1253bae560f59c7a775800809aa
Nayla Rizkia <naylarizkvia@gmail.com>
2026-04-22 00:11:31 +0800

    Update prob2.cpp

--- prob2.cpp
+++ prob2.cpp
@@ -1,1 +1,23 @@
 #include "queue.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int arr[105]; 
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+   if (k <= 0 || k > n) {
+        return 0;
+    }
+
+    Queue q;
+    init(&q);
+
+    int current_sum = 0;
+}
